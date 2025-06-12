d08231289648c05ab5b5328f06df61eb4e4130e9
Riyas khan <m.driyaskhan55@gmail.com>
2025-06-12 20:07:07 +0530

    comment app

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     with application.app_context():
         db.create_all()
-    application.run(debug=True)
+    # application.run(debug=True)
