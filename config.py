252469dc69b7efa7d5a912ae58c5e8dddf8d2c8a
Riyas khan <m.driyaskhan55@gmail.com>
2025-06-12 21:11:12 +0530

    url added

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key_here')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SQLALCHEMY_ECHO = False  
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:Riyas123@localhost/customer')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:Riyas123@localhost/aalimas_db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://RsjRiyas:Riyas123@RsjRiyas.mysql.pythonanywhere-services.com/RsjRiyas$aalimas_db')
