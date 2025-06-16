9f7359e8e716d3f595ae684b3d27aa3b0973ace8
Riyas khan <m.driyaskhan55@gmail.com>
2025-06-16 21:43:49 +0530

    pass

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SQLALCHEMY_ECHO = False  
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:Riyas123@localhost/customer')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://Aalimas:Riyas123$@Aalimas.mysql.pythonanywhere-services.com/Aalimas$aalimas_db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://Aalimas:Riyas123@Aalimas.mysql.pythonanywhere-services.com/Aalimas$aalimas_db')
