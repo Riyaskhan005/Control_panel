2fbdcf1d9d18e58902976834b0d133fd00070129
Riyas khan <m.driyaskhan55@gmail.com>
2025-06-16 21:23:04 +0530

    updated

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key_here')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SQLALCHEMY_ECHO = False  
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:Riyas123@localhost/customer')
